7b80f4b079b5ef5d0272dcfccf98568bac5483cd
Asrid <akoredeogunsola452@gmail.com>
2024-07-23 11:58:33 -0700

    first project

---
+++ bio.js
@@ -0,0 +1,10 @@
+var  fullname = prompt("what is your full name?");
+ alert("you are welcome to my page" + fullname + "enjoy yourself");
+var favorite = prompt("what is your favorite drink?");
+ alert("great,drinking" + favorite + "can help quench taste");
+var school = prompt("which school do you attended?");
+ alert("great!," + school + "graduates are wonderfull");
+var  president = prompt("who is your president?");
+ alert("president" + president + "is a good man");
+var  occupation = prompt("what is your president?");
+ alert("great!" + occupation + "");
